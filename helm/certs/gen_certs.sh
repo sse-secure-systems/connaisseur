047bcd831cdd61e5238d6ef05af70ecf415e092b
Philipp Belitz <philipp.belitz@securesystems.de>
2020-09-11 17:43:33 +0200

    fix: added subjectAltNames to TLS certificates
    Kubernetes v1.19 requires subjectAltNames to be present on the TLS certificates or otherwise will break. Even though the TLS config file already added those, an OpenSSL bug prevented them to be actually propagated to the actual certificates.
    
    closes #4

--- helm/certs/gen_certs.sh
+++ helm/certs/gen_certs.sh
@@ -4,40 +4,17 @@ SCRIPT_PATH=$(dirname $0)
 SERVICE_NAME=$(yq r $SCRIPT_PATH/../Chart.yaml name)
 
 cd $SCRIPT_PATH
-openssl genrsa -out ca.key 4096
-
-cat <<EOF > ca.conf
-[ req ]
-default_bits       = 4096
-default_md         = sha512
-default_keyfile    = ca.key
-prompt             = no
-encrypt_key        = yes
-
-# base request
-distinguished_name = req_distinguished_name
-
-# distinguished_name
-[ req_distinguished_name ]
-countryName            = "DE"
-stateOrProvinceName    = "Berlin"
-localityName           = "Berlin"
-organizationName       = "SSE"
-organizationalUnitName = "Engineering Department"
-commonName             = "connaisseur.internal"
-emailAddress           = "no-reply@securesystems.de"
-EOF
-openssl req -new -x509 -key ca.key -out ca.crt -config ca.conf
-
-openssl genrsa -out tls-key.pem 4096
-cat <<EOF > tls.conf
+openssl genrsa -out tls.key 4096
+cat <<EOF > tls-csr.conf
 [req]
 req_extensions = v3_req
 distinguished_name = req_distinguished_name
+
 [ req_distinguished_name ]
+
 [ v3_req ]
-basicConstraints=CA:FALSE
-subjectAltName=@alt_names
+basicConstraints = CA:FALSE
+subjectAltName = @alt_names
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 extendedKeyUsage = serverAuth
 
@@ -47,5 +24,20 @@ DNS.2 = $SERVICE_NAME-svc.$SERVICE_NAME
 DNS.3 = $SERVICE_NAME-svc.$SERVICE_NAME.svc
 DNS.4 = $SERVICE_NAME-svc.$SERVICE_NAME.svc.cluster.local
 EOF
-openssl req -new -key tls-key.pem -subj "/CN=$SERVICE_NAME-svc.$SERVICE_NAME.svc" -out tls.csr -config tls.conf
-openssl x509 -req -in tls.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out tls-crt.pem
+
+cat <<EOF > tls-crt.conf
+[ v3_ca]
+basicConstraints = CA:FALSE
+subjectAltName = @alt_names
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
+
+[ alt_names ]
+DNS.1 = $SERVICE_NAME-svc
+DNS.2 = $SERVICE_NAME-svc.$SERVICE_NAME
+DNS.3 = $SERVICE_NAME-svc.$SERVICE_NAME.svc
+DNS.4 = $SERVICE_NAME-svc.$SERVICE_NAME.svc.cluster.local
+EOF
+
+openssl req -new -key tls.key -sha256 -config tls-csr.conf -subj "/CN=$SERVICE_NAME-svc.$SERVICE_NAME.svc" -out tls.csr
+openssl x509 -req -days 36500 -in tls.csr -signkey tls.key -sha256 -extensions v3_ca -extfile tls-crt.conf -out tls.crt
