42288ef25356fe09a37e4e3ca641eee2bd9df34e
Philipp Belitz <philipp.belitz@securesystems.de>
2021-05-04 14:49:00 +0200

    test: adjusted tests

--- tests/validators/notaryv1/test_notary.py
+++ tests/validators/notaryv1/test_notary.py
@@ -157,8 +157,8 @@ def test_get_trust_data(
     with exception:
         no = notary.Notary(**sample_notaries[index])
         td = no.get_trust_data(Image(image), role)
-        assert td.signed == output["signed"]
-        assert td.signatures == output["signatures"]
+        assert td["signed"] == output["signed"]
+        assert td["signatures"] == output["signatures"]
 
 
 @pytest.mark.parametrize(
