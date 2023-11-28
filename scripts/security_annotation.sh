db33baef7b5f939b55bfb0ab9e3a86480a140a8c
Teetje Stark <teetje.stark@gmx.de>
2023-11-28 10:49:24 +0100

    ci: Add security release annotation if there is a commit with security commit header

---
+++ scripts/security_annotation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# Running as Shell such that we don't need to install dependencies in last pipeline step
+
+set -eu
+
+DIFF_REF=$(git tag --sort=version:refname | tail -n2 | sed 'N;s/\n/.../')
+COUNT=$(git log "${DIFF_REF}" --no-decorate --pretty=%s | cut -d ":" -f1 | grep -c sec || true)
+
+if [[ ${COUNT} -gt 0 ]]; then
+  echo """
+annotations:
+  artifacthub.io/containsSecurityUpdates: "true"
+""" >> helm/Chart.yaml
+fi
