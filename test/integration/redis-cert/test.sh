85943cad2b4bb86e5cd9ba76b9e4b1349f76d47b
Philipp Belitz <philipp.belitz@securesystems.de>
2024-08-02 11:45:05 +0200

    test: fix flakey redis-cert test
    The redis cert test tested that after upgrading a redis with a custom certificate the caching mechanism still worked. Problem was, the redis instance might have been ready on Kubernetes side, but internally not ready to receive connections. Thus the caching was tested and failed, before the newly upgraded redis was ready. This has been fixed with a wating mechanism.

--- test/integration/redis-cert/test.sh
+++ test/integration/redis-cert/test.sh
@@ -6,6 +6,9 @@ redis_cert_test() {
     update_with_file "redis-cert/install.yaml"
     install "make"
 
+    # wait for redis to be ready
+    wait_for_redis
+
     # create same image twice
     single_test "redis-cert-01-signed" "Testing signed nv1 image..." "deploy" "securesystemsengineering/testimage:signed" "default" "" "null"
     single_test "redis-cert-02-cached" "Testing signed nv1 image using cache..." "deploy" "securesystemsengineering/testimage:signed" "default" "" "null"
@@ -24,6 +27,9 @@ redis_cert_test() {
     update_with_file "redis-cert/update.yaml"
     upgrade "make"
 
+    # wait for redis to be ready
+    wait_for_redis
+
     # create same image twice
     single_test "redis-cert-03-signed" "Testing signed nv1 image again..." "deploy" "securesystemsengineering/testimage:signed" "default" "" "null"
     single_test "redis-cert-04-cached" "Testing signed nv1 image using cache again..." "deploy" "securesystemsengineering/testimage:signed" "default" "" "null"
@@ -38,4 +44,13 @@ redis_cert_test() {
         EXIT="1"
     fi
     rm output.log
+}
+
+wait_for_redis() {
+    timeout 30 bash -c '
+    while true; do
+        kubectl logs -n connaisseur -lapp.kubernetes.io/instance=redis --tail=-1 | grep -q  "Ready to accept connections tls" && break
+        sleep 1
+    done
+    ' || true
 }
